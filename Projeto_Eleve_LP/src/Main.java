507e3870ad4742217529a6474943317f7aea8418
Leonardo <Leonardo@Leonardo-PC>
2013-09-28 09:38:47 -0300

    primeiro commit

--- Projeto_Eleve_LP/src/Main.java
+++ Projeto_Eleve_LP/src/Main.java
@@ -13,6 +13,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ELEVE DHL!");
+        System.out.println("ELEVE DHL Leo & Luis!");
     }
 }
