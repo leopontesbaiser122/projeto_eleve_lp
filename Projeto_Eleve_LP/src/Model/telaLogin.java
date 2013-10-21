850ae657fd5569fb8e956b0543626cddf178f396
Leonardo <Leonardo@Leonardo-PC>
2013-10-21 09:55:55 -0200

    projetos

--- Projeto_Eleve_LP/src/Model/telaLogin.java
+++ Projeto_Eleve_LP/src/Model/telaLogin.java
@@ -1,47 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Model;
 
-import java.awt.*;
-import javax.swing.*;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
+/**
+ *
+ * @author Leonardo
+ */
+public class telaLogin {
 
-public class telaLogin extends JFrame {
-
-    JTextField txtLogin = new JTextField();
-    JTextField txtSenha = new JTextField();
-    public telaLogin() {
-        super("Eleve Desenvolvimento Humano Ltda");
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        Container c = this.getContentPane();
-
-        GridBagConstraints cons = new GridBagConstraints();
-        GridBagLayout layout = new GridBagLayout();
-        c.setLayout(layout);
-
-        cons.fill = GridBagConstraints.BOTH;
-        cons.gridy = 0;
-        cons.gridx = 0;
-        cons.gridwidth = 2;
-        c.add(txtLogin, cons);
-        cons.gridwidth = 1;
-        cons.gridx = 2;
-        c.add(new JButton("Direita"), cons);
-
-        cons.gridx = 0;
-        cons.gridy = 1;
-        c.add(new JButton("Esquerda"), cons);
-
-        cons.gridx = 1;
-        c.add(new JButton("Meio"), cons);
-
-        cons.gridx = 2;
-        c.add(new JButton("Direita"), cons);
-
-        this.setSize(600, 600);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
-
-//    public static void main(String[] args) {
-//        telaLogin exe = new telaLogin();
-//        exe.setVisible(true);
-//    }
 }
